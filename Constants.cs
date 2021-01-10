10960185123203311cd7395585cb2beab55ff2b0
BinyT <bitad_66@hotmail.com>
2021-01-10 12:57:21 -0500

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace AzureAPIs
+{
+    public static class Constants
+    {
+        public const string CategoryLabel = "category";
+        public const string UploadedByLabel = "uploadedBy";
+        public const string DateLabel = "date";
+        public const string UserRoleLabel = "userRole";
+        public const string ServiceImplIdLabel = "serviceImplId";
+        public const string OrganizationUnitIdLabel = "organizationUnitId";
+        public const string IndicatorCodeLabel = "indicatorCode";
+        public const string FileDescriptionLabel = "fileDescription";
+    }
+}
---
+++ MyBlobFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AzureAPIs
+{
+    public class MyBlobFile
+    {
+        public byte[] FileContent { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Category { get; set; }
+
+        public string UploadedBy { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string UserRole { get; set; }
+    }
+}
